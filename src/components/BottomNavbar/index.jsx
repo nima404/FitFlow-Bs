013c64343bee5fd544f13ca338c21b64eddde5d4
nima tajik <nima.error404@gmail.com>
2023-05-13 16:25:31 +0330

    update: change bottom-navbar data , added profile to dashboard page

--- src/components/BottomNavbar/index.jsx
+++ src/components/BottomNavbar/index.jsx
@@ -1,26 +1,30 @@
 import { Col, Container, Row } from "react-bootstrap"
 import './style.css';
 import dashboard from '../../assets/icons/dashboard.png';
+import user from '../../assets/icons/user.png';
+import users from '../../assets/icons/users.png';
+import setting from '../../assets/icons/setting.png';
 
 export const BottomNavbar = () => {
+
     return (
-        <Container className="position-fixed bottom-0 start-0 w-100">
-            <Row className="d-flex justify-content-betweeb align-items-center py-2">
+        <Container className="position-fixed bottom-0 start-0 w-100 bottom_menu_bg">
+            <Row className="d-flex justify-content-between align-items-center p-2">
                 <Col className="d-flex flex-column align-items-center justify-content-center">
                     <img src={dashboard} />
                     <h5 className="mt-2 optionTitle">Dashboard</h5>
                 </Col>
                 <Col className="d-flex flex-column align-items-center justify-content-center">
-                    <img src={dashboard} />
-                    <h5 className="mt-2 optionTitle">Dashboard</h5>
+                    <img src={users} />
+                    <h5 className="mt-2 optionTitle">vrienden</h5>
                 </Col>
                 <Col className="d-flex flex-column align-items-center justify-content-center">
-                    <img src={dashboard} />
-                    <h5 className="mt-2 optionTitle">Dashboard</h5>
+                    <img src={user} />
+                    <h5 className="mt-2 optionTitle">coaches</h5>
                 </Col>
                 <Col className="d-flex flex-column align-items-center justify-content-center">
-                    <img src={dashboard} />
-                    <h5 className="mt-2 optionTitle">Dashboard</h5>
+                    <img src={setting} />
+                    <h5 className="mt-2 optionTitle">Instellingen</h5>
                 </Col>
             </Row>
         </Container>
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react"
 import { Sidebar } from "../../components/Sidebar"
-import { Container } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import { BottomNavbar } from "../../components/BottomNavbar"
+import './style.css';
+import logo from '../../assets/images/logo.png';
+import sidebar from '../../assets/icons/sidebar.png';
+import profile from '../../assets/images/profile.png';
 
 export const Dashboard = () => {
     const [sidebarState, setSidebarState] = useState(false)
@@ -10,8 +14,28 @@ export const Dashboard = () => {
             <Sidebar state={sidebarState} setState={setSidebarState} />
 
             <Container>
-                <button onClick={() => setSidebarState(true)}>clicl</button>
-            </Container>
+                <Row className="mt-4 d-flex justify-content-between align-items-center">
+                    <Col>
+                        <img src={logo} className="logoStyle" />
+                    </Col>
+                    <Col className="d-flex justify-content-end">
+                        <img src={sidebar} onClick={() => setSidebarState(true)} />
+                    </Col>
+                </Row>
+
+                <Row className="mt-5 mb-3">
+                    <Col className=" mt-4 d-flex justify-content-center align-items-center flex-column">
+                        <h5 className="d-flex justify-content-center align-items-center mb-3 welcomeText">welkom Ashley!</h5>
+                        <div className="profile_border p-2 rounded-circle">
+                            <img src={profile} />
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row>
+                    
+                </Row>
+            </Container >
 
             <BottomNavbar />
         </>
